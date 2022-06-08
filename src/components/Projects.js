8f38a9ba5794a5cfd4e07fe18f6da94ee7dab627
MiguelHdez18 <miguel.hernandez@ithealth.co>
2022-06-08 11:47:01 -0500

    updateprojects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,8 @@ import guajolotas from "../img/guajolotas.PNG";
 import pokedex from "../img/pokedex.PNG";
 import movies from "../img/movies.PNG";
 import utest from "../img/utest.PNG"
+import bokeein from '../img/bokkein.PNG'
+import gifsapp from '../img/libreriagifs.PNG'
 
 const Projects = () => {
   return (
@@ -14,7 +16,7 @@ const Projects = () => {
       <h1 class="text-center text-4xl tracking-tight font-extrabold text-gray-800 sm:text-5xl md:text-6xl">
         <span class=" block text-indigo-600 xl:inline"> Projects</span>
       </h1>
-      <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
+      <div className="container px-5 py-8 mx-auto lg:pt-12 lg:px-32">
         <div className="flex flex-wrap -m-1 md:-m-2">
           <div className="flex flex-wrap w-1/3">
             <div className="w-full p-1 md:p-2">
@@ -68,6 +70,30 @@ const Projects = () => {
                   alt="gallery"
                   className="block object-cover object-center w-full h-full rounded-lg"
                   src={utest}
+                />
+              </a>
+            </div>
+          </div>
+
+          <div className="flex flex-wrap w-1/3">
+            <div className="w-full p-1 md:p-2">
+              <a href="https://bookein.vercel.app/">
+                <img
+                  alt="gallery"
+                  className="block object-cover object-center w-full h-full rounded-lg"
+                  src={bokeein}
+                />
+              </a>
+            </div>
+          </div>
+
+          <div className="flex flex-wrap w-1/3">
+            <div className="w-full p-1 md:p-2">
+              <a href="https://gifs-app-one.vercel.app/">
+                <img
+                  alt="gallery"
+                  className="block object-cover object-center w-full h-full rounded-lg"
+                  src={gifsapp}
                 />
               </a>
             </div>
